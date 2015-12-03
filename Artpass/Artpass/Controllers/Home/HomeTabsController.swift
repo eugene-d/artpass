0ca1fcd34db6dd4fcf4b10a0443ee44bdff25fe9
Evgeniy D <promocoding@gmail.com>
2015-12-03 09:34:28 +0200

    Make Event list tab active by default

--- Artpass/Artpass/Controllers/Home/HomeTabsController.swift
+++ Artpass/Artpass/Controllers/Home/HomeTabsController.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class HomeTabsController: UITabBarController {
     
+    let defaultActiveTabIndex = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        makeDefaultTabActive()
+    }
+    
+    func makeDefaultTabActive() {
+        if let viewControllersCount = viewControllers?.count
+            where viewControllersCount - 1 >= defaultActiveTabIndex {
+                self.selectedViewController = viewControllers?[defaultActiveTabIndex]
+        }
+    }
 }
