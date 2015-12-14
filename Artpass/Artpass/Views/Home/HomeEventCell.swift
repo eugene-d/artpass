e28155c65f2895a1c84f43c26f333f85351c13c0
Evgeniy D <promocoding@gmail.com>
2015-12-14 23:45:33 +0200

    Polist events table on home view

--- Artpass/Artpass/Views/Home/HomeEventCell.swift
+++ Artpass/Artpass/Views/Home/HomeEventCell.swift
@@ -5,6 +5,21 @@ class HomeEventCell: UITableViewCell {
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var cityLabel: UILabel!
     
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+        if #available(iOS 9, *) {
+            self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        } else {
+//            self.titleLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
